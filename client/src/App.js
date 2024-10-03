8ee705edcd35a06514c167638dc0182747db68e8
vedantbhatotia <vedantbhatotia@gmail.com>
2024-10-03 21:13:19 +0530

    frontend with apollo client

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import {ApolloClient,InMemoryCache,ApolloProvider} from "@apollo/client";
+
+function App() {
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri:"http://localhost:4000/graphql"
+  })
+
+  return (
+    <ApolloProvider client={client}>
+      <div className="App">
+        <h1>List Of Users</h1>
+
+      </div>
+    </ApolloProvider>
+
+  );
+}
+
+export default App;
